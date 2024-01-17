4e432e40fae4a0e95553e10ec6627276b976f613
Justin Jung <justin@bustinbung.com>
2024-01-16 23:30:09 -0500

    [alacritty] Delete theme script, I'm using kitty

--- run_once_install-alacritty-themes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -d ~/.config/alacritty/themes ]]; then
-    mkdir -p ~/.config/alacritty/themes
-    git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-fi
