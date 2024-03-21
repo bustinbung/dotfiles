bdf26be82d4af449dbef30428a50ffdfbb4f3b35
Justin Jung <justin@bustinbung.com>
2024-03-20 21:54:31 -0400

    [chezmoi] Make look pretty

--- packages/arch-linux-install.sh
+++ packages/arch-linux-install.sh
@@ -1,25 +1,42 @@
 # Install arch packages
-sudo pacman -S --noconfirm --needed - < arch-pkglist
+echo ┌──────────────────────────┐
+echo │ Installing Arch packages │
+echo └──────────────────────────┘
 sudo pacman -S --noconfirm --needed - < $HOME/.local/share/chezmoi/packages/arch-pkglist
 
 # Install AUR packages
-yay -S --noconfirm --needed - < aur-pkglist
+echo ┌─────────────────────────┐
+echo │ Installing AUR packages │
+echo └─────────────────────────┘
 yay -S --noconfirm --needed - < $HOME/.local/share/chezmoi/packages/aur-pkglist
 
 # Fix suspend issues
 if [ ! -e /etc/tmpfiles.d/fix-suspend.conf ] ; then
+  echo ┌─────────────────────────────┐
+  echo │ Writing suspend fix tmpfile │
+  echo └─────────────────────────────┘
   echo w /proc/acpi/wakeup - - - - GPP0 | sudo tee /etc/tmpfiles.d/fix-suspend.conf
+
+  echo ┌────────────────────────────────┐
+  echo │ Enabling Nvidia power services │
+  echo └────────────────────────────────┘
+  sudo systemctl enable nvidia-{suspend,hibernate,resume}
 fi
-sudo systemctl enable nvidia-{suspend,hibernate,resume}
 
 # Install doom emacs
 if [ ! -d $HOME/.config/emacs ] ; then
+  echo ┌───────────────────────┐
+  echo │ Installing Doom Emacs │
+  echo └───────────────────────┘
   git clone --depth 1 git@github.com:doomemacs/doomemacs $HOME/.config/emacs
   $HOME/.config/emacs/bin/doom install --force --config --env --install --fonts
 fi
 
 # Install SDKMAN
 if [ ! -d $HOME/.sdkman ] ; then
+  echo ┌───────────────────┐
+  echo │ Installing SDKMAN │
+  echo └───────────────────┘
   curl -s "https://get.sdkman.io" | bash
   source $HOME/.sdkman/bin/sdkman-init.sh
 
@@ -29,5 +46,8 @@ fi
 
 # Change user shell
 if [ ! $SHELL = $(which zsh) ] ; then
+  echo ┌───────────────────────┐
+  echo │ Changing shell to zsh │
+  echo └───────────────────────┘
   sudo chsh -s $(which zsh) $(whoami)
 fi
--- packages/macos-install.sh
+++ packages/macos-install.sh
@@ -1,2 +1,5 @@
 # Install packages
+echo ┌──────────────────────────────┐
+echo │ Installing homebrew packages │
+echo └──────────────────────────────┘
 brew bundle install --no-lock --file=~/.local/share/chezmoi/packages/Brewfile
