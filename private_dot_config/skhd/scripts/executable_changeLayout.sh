b68c92d57b63b0d89813530e5485596a47306d2e
Justin Jung <justin@bustinbung.com>
2024-03-25 23:44:46 -0400

    [skhd] Remove float from space options

--- private_dot_config/skhd/scripts/executable_changeLayout.sh
+++ private_dot_config/skhd/scripts/executable_changeLayout.sh
@@ -2,18 +2,10 @@
 
 LAYOUT=$(yabai -m query --spaces --space | jq '.type')
 
-if [[ $1 == -r ]]; then
-    case $LAYOUT in
-        "\"bsp\"") yabai -m space --layout float ;;
-        "\"float\"") yabai -m space --layout stack ;;
-        "\"stack\"") yabai -m space --layout bsp && yabai -m space --mirror y-axis ;;
-    esac
-else
-    case $LAYOUT in
-        "\"bsp\"") yabai -m space --layout stack ;;
-        "\"float\"") yabai -m space --layout bsp && yabai -m space --mirror y-axis ;;
-        "\"stack\"") yabai -m space --layout float ;;
-    esac
-fi
+case $LAYOUT in
+    "\"bsp\"") yabai -m space --layout stack ;;
+    "\"stack\"") yabai -m space --layout bsp ;;
+    *) yabai -m space --layout stack
+esac
 
 sketchybar --trigger layout_change
