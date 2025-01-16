839d4a0b94db5b11831e227fc1df0dc2101dcc47
Justin Jung <justin@bustinbung.com>
2025-01-16 00:53:07 -0500

    Remove old files

--- scripts/macos-install.sh
+++
@@ -1,5 +0,0 @@
-# Install packages
-echo ┌──────────────────────────────┐
-echo │ Installing homebrew packages │
-echo └──────────────────────────────┘
-brew bundle install --no-lock --file=~/.local/share/chezmoi/packages/Brewfile
