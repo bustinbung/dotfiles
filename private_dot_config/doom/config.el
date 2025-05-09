9ae0017674fd2d4481e0c9f09789f71c4bc3a1ec
Justin Jung <justin@bustinbung.com>
2025-05-08 23:57:24 -0400

    [doom] Bring back some old settings

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -40,7 +40,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Nextcloud/org/"
+      org-agenda-files (directory-files-recursively "~/Nextcloud/org" "\\.org$"))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -74,3 +75,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq scroll-margin 10)
+
